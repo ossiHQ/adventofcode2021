313918882a9efe82e310db2052c20a700b30ab2c
ossiHQ <77418402+ossiHQ@users.noreply.github.com>
2021-12-10 16:03:31 +0100

    Create day2_additional.java

---
+++ day2/day2_additional.java
@@ -0,0 +1,52 @@
+
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day2 {
+
+    public static void run() {
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+  
+        // example input
+        String input =
+                """
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+                """;
+
+        List<String> list = Arrays.asList(input.split("\n"));
+
+        for(int i = 0; i < list.size(); i++) {
+            var index = list.get(i).indexOf(' ');
+
+            String instruction = list.get(i).substring(0, index);
+            int size = Integer.parseInt(list.get(i).substring(index + 1));
+
+            switch (instruction) {
+                case "forward":
+                    horizontal += size;
+                    depth += aim * size;
+                    break;
+                case "down":
+                    aim += size;
+                    break;
+                case "up":
+                    aim -= size;
+                    break;
+            }
+           
+        }
+
+        System.out.print("Depth:" + depth + "\n" + "Horizontal: " + horizontal + "\n" + "Together:" + (horizontal * depth));
+
+    }
+}
